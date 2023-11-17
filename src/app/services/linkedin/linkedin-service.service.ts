ff9485f8de871e8b5de45316299716cfc6568ece
Adarsh <adarshsingh2k@gmail.com>
2023-11-17 13:31:57 +0530

    skills format changed

--- src/app/services/linkedin/linkedin-service.service.ts
+++ src/app/services/linkedin/linkedin-service.service.ts
@@ -1,35 +1,38 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LinkedinServiceService {
-  username='adarshsingh2k'
-  constructor(private http:HttpClient) { }
+  username = 'adarshsingh2k';
+  constructor(private http: HttpClient) {}
 
   // we can get data either from api and if api has error then get data from json
-  public getLinkedinDataApi(){
-    const httpHeaders=new HttpHeaders({
-    //   'X-RapidAPI-Key': '226e0cc442mshb0890657c3054afp1db7a3jsn53e0f518c8f6',
-		// 'X-RapidAPI-Host': 'linkedin-profile-data.p.rapidapi.com'
-    'X-RapidAPI-Key': '226e0cc442mshb0890657c3054afp1db7a3jsn53e0f518c8f6',
-		'X-RapidAPI-Host': 'fresh-linkedin-profile-data.p.rapidapi.com'
+  public getLinkedinDataApi() {
+    const httpHeaders = new HttpHeaders({
+      //   'X-RapidAPI-Key': '226e0cc442mshb0890657c3054afp1db7a3jsn53e0f518c8f6',
+      // 'X-RapidAPI-Host': 'linkedin-profile-data.p.rapidapi.com'
+      'X-RapidAPI-Key': '226e0cc442mshb0890657c3054afp1db7a3jsn53e0f518c8f6',
+      'X-RapidAPI-Host': 'fresh-linkedin-profile-data.p.rapidapi.com',
     });
 
-    const url = `https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2F${this.username}%2F`;
-    return this.http.get(url,{
-      headers:httpHeaders
-    })
-    .pipe(
-      catchError(this.handleError)
-    );
+    const url = `https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2F${this.username}%2F&include_skills=true`;
+    return this.http
+      .get(url, {
+        headers: httpHeaders,
+      })
+      .pipe(catchError(this.handleError));
   }
 
-  public getLinkedinDataJson(){
-    return this.http.get("../../../assets/linkedin_data.json")
+  public getLinkedinDataJson() {
+    return this.http.get('../../../assets/linkedin_data.json');
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -39,18 +42,10 @@ export class LinkedinServiceService {
     } else {
       // Server-side error
       console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
       );
     }
     // Throw an observable with a user-facing error message
     return throwError('Something went wrong. Please try again later.');
   }
- 
-  
-  
-  
-  
-  
-
 }
